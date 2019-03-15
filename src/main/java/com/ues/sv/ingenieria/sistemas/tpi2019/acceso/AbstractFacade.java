0602c4ce65058de6c52b3a37944de2ffaa97859d
DeadBryam <bryamfeik@gmail.com>
2019-03-15 10:12:25 -0600

    Se terminaron pruebas, se corrigieron metodos en el AbstractFacade. Aun falta aplicar patron.

--- src/main/java/com/ues/sv/ingenieria/sistemas/tpi2019/acceso/AbstractFacade.java
+++ src/main/java/com/ues/sv/ingenieria/sistemas/tpi2019/acceso/AbstractFacade.java
@@ -6,6 +6,8 @@
 package com.ues.sv.ingenieria.sistemas.tpi2019.acceso;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 
 /**
@@ -30,9 +32,13 @@ public abstract class AbstractFacade<T> {
         try {
             if (getEntityManager() != null && entity != null) {
                 getEntityManager().persist(entity);
-            }   
+            } else {
+                Logger.getLogger(getClass().getName()).log(Level.WARNING, "EntityManager o Entity son nulos");
+                throw new NullPointerException("EntityManager o Entity son nulos");
+            }
         } catch (Exception e) {
-            System.out.println("ERROR: " + e);
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            throw e;
         }
     }
 
@@ -45,10 +51,12 @@ public abstract class AbstractFacade<T> {
             if (getEntityManager() != null && entity != null) {
                 getEntityManager().merge(entity);
             } else {
-                System.out.println("algo es nulo");
+                Logger.getLogger(getClass().getName()).log(Level.WARNING, "EntityManager o Entity son nulos");
+                throw new NullPointerException("EntityManager o Entity son nulos");
             }
         } catch (Exception e) {
-            System.out.println("ERROR: " + e);
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            throw e;
         }
     }
 
@@ -61,10 +69,12 @@ public abstract class AbstractFacade<T> {
             if (getEntityManager() != null && entity != null) {
                 getEntityManager().remove(getEntityManager().merge(entity));
             } else {
-                System.out.println("algo es nulo");
+                Logger.getLogger(getClass().getName()).log(Level.WARNING, "EntityManager o Entity son nulos");
+                throw new NullPointerException("EntityManager o Entity son nulos");
             }
         } catch (Exception e) {
-            System.out.println("ERROR: " + e);
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+            throw e;
         }
     }
 
@@ -91,12 +101,12 @@ public abstract class AbstractFacade<T> {
         return getEntityManager().createQuery(cq).getResultList();
     }
 
-    public List<T> findRange(int[] range) {
+    public List<T> findRange(int primero, int ultimo) {
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         cq.select(cq.from(entityClass));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
-        q.setMaxResults(range[1] - range[0] + 1);
-        q.setFirstResult(range[0]);
+        q.setMaxResults(ultimo);
+        q.setFirstResult(primero);
         return q.getResultList();
     }
 
@@ -106,6 +116,7 @@ public abstract class AbstractFacade<T> {
         cq.select(getEntityManager().getCriteriaBuilder().count(rt));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
+        
     }
 
 }
--- src/test/java/com/ues/sv/ingenieria/sistemas/tpi2019/acceso/DistribuidorFacadeTest.java
+++ src/test/java/com/ues/sv/ingenieria/sistemas/tpi2019/acceso/DistribuidorFacadeTest.java
@@ -8,16 +8,18 @@ package com.ues.sv.ingenieria.sistemas.tpi2019.acceso;
 import com.ues.sv.ingenieria.sistemas.tpi2019.entities.Distribuidor;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Selection;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.mockito.Matchers;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.internal.util.reflection.Whitebox;
 
 /**
@@ -27,59 +29,124 @@ import org.mockito.internal.util.reflection.Whitebox;
 public class DistribuidorFacadeTest {
 
     private final DistribuidorFacade cut = new DistribuidorFacade();
+    private final DistribuidorFacade nor = new DistribuidorFacade();
     private final Distribuidor esperado = new Distribuidor(Short.parseShort("1"));
+    private final CriteriaBuilder CB = mock(CriteriaBuilder.class);
+    private final CriteriaQuery CQ = mock(CriteriaQuery.class);
+    private final TypedQuery q = mock(TypedQuery.class);
     private Distribuidor resultado;
     private EntityManager em;
     private List<Distribuidor> lstEsperado;
 
     @Before
     public void init() {
-        em = Mockito.mock(EntityManager.class);
-        //cut.em = this.em;
+        em = mock(EntityManager.class);
         Whitebox.setInternalState(cut, "em", em);
-     
+
+        when(this.em.getCriteriaBuilder()).thenReturn(CB);
+        when(CB.createQuery()).thenReturn(CQ);
+        when(CQ.select(Matchers.any(Selection.class))).thenReturn(CQ);
+        when(this.em.createQuery(CQ)).thenReturn(q);
+
     }
 
+    /**
+     * Test of create method, of class AbstractFacade.
+     */
     @Test(expected = Exception.class)
-    public void testFindById(){
-        System.out.println("testFindById");
-        //Whitebox.setInternalState(cut, "em", ema);
+    public void testCreate() {
+        cut.create(esperado);
+        verify(em).persist(esperado);
 
-        Mockito.when(this.em.find(Distribuidor.class, 1)).thenReturn(esperado);
+        cut.create(null);
+
+        nor.create(esperado);
+    }
+
+    /**
+     * Test of create method, of class AbstractFacade.
+     */
+    @Test(expected = Exception.class)
+    public void testEdit() {
+        cut.edit(esperado);
+        verify(em).merge(esperado);
+
+        cut.edit(null);
+
+        nor.edit(esperado);
+    }
+
+    /**
+     * Test of create method, of class AbstractFacade.
+     */
+    @Test(expected = Exception.class)
+    public void testRemove() {
+        cut.remove(esperado);
+        verify(em).remove(em.merge(esperado));
+
+        cut.remove(null);
+
+        nor.remove(esperado);
+    }
+
+    /**
+     * Test of findById method, of class AbstractFacade.
+     */
+    @Test(expected = Exception.class)
+    public void testFindById() {
+        System.out.println("testFindById");
+
+        when(this.em.find(Distribuidor.class, 1)).thenReturn(esperado);
 
         resultado = cut.findById(1);
         assertEquals(esperado, resultado);
 
         resultado = cut.findById(null);
         assertNull(resultado);
-            
-        new DistribuidorFacade().findById("error"); 
+
+        nor.findById("error");
     }
-    
+
+    /**
+     * Test of findAll method, of class AbstractFacade.
+     */
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         System.out.println("testFindAll");
         List<Distribuidor> lstResultado;
-        
-        CriteriaBuilder CB = Mockito.mock(CriteriaBuilder.class);
-        CriteriaQuery CQ = Mockito.mock(CriteriaQuery.class);
-        TypedQuery q = Mockito.mock(TypedQuery.class);
-        
-        Mockito.when(this.em.getCriteriaBuilder()).thenReturn(CB);
-        Mockito.when(CB.createQuery()).thenReturn(CQ);
-        Mockito.when(CQ.select(Matchers.any(Selection.class))).thenReturn(CQ);
-        Mockito.when(this.em.createQuery(CQ)).thenReturn(q);
-        Mockito.when(q.getResultList()).thenReturn(lstEsperado);
-        
+
+        when(q.getResultList()).thenReturn(lstEsperado);
         lstResultado = cut.findAll();
-        
+
         assertEquals(lstResultado, lstEsperado);
-        
     }
-    
+
+    /**
+     * Test of findRange method, of class AbstractFacade.
+     */
     @Test
-    public void testCreate(){
-        
-        
+    public void testFindRange() {
+        System.out.println("testFindRange");
+        List<Distribuidor> lstResultado;
+
+        when(q.getResultList()).thenReturn(lstEsperado);
+        lstResultado = cut.findRange(0, 100);
+
+        assertEquals(lstResultado, lstEsperado);
     }
+
+    /**
+     * Test of count method, of class AbstractFacade.
+     */
+    @Test
+    public void testCount() {
+        System.err.println("testCount");
+        int res;
+
+        when(q.getSingleResult()).thenReturn((Long) 0l);
+        res = cut.count();
+
+        assertEquals(0, res);
+    }
+
 }
